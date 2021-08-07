6e3bae30515d9e17e497eb7dcfb367fe526ec508
Mohammed Shaz <shelly69shock.com>
2021-08-07 20:59:44 +0530

    here we go again

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.sendFile('./index.html')
+    res.sendFile(process.cwd()+'/index.html')
 })
 
 app.listen(3000, () => {
