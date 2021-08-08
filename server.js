e9082f947a26032736f5fd391128c8c78b7fc57f
Mohammed Shaz <shelly69shock.com>
2021-08-08 15:32:58 +0530

    come aan

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
     res.sendFile(process.cwd()+'/index.html')
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server running on port:3000')
 })
 
